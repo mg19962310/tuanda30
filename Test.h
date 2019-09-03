97839d8a2105214b7007fd97510426f615137fa0
mg2310 <mg2310>
2019-09-03 22:37:11 +0700

    Add Test.h

---
+++ Test.h
@@ -0,0 +1,13 @@
+#ifndef TEST
+#define TEST
+#include<iostream>
+#include<string>
+using namespace std;
+class Test{
+  private:
+    string name;
+  public:
+    Test(string a);
+    void showName();
+};
+#endif
